221a8ca7d8dc4c3ca10292c0fba9c2013b1086fe
senthil100695 <senthil100695@gmail.com>
2023-10-09 13:41:01 +0530

    base model added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import (DataIngestionTrainingPipeline)
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -12,3 +13,13 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Prepare Base Model Stage"
+
+try:
+        logger.info(f">>>>>>> Stage {STAGE_NAME} Started <<<<<<<")
+        prepare_base_model = PrepareBaseModelTrainingPipeline()
+        prepare_base_model.main()
+        logger.info(f">>>>>>> Stage {STAGE_NAME} Completed <<<<<<<")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,7 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -28,3 +29,24 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        params = self.params
+
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=config.root_dir,
+            base_model_path=config.base_model_path,
+            updated_base_model_path=config.updated_base_model_path,
+            params_image_size = params.IMAGE_SIZE,
+            params_learning_rate = params.LEARNING_RATE,
+            params_include_top = params.INCLUDE_TOP,
+            params_weights = params.WEIGHTS,
+            params_classes = params.CLASSES,
+            params_batch_size = params.BATCH_SIZE
+            
+        )
+
+        return prepare_base_model_config
